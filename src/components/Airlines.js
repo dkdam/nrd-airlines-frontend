3e43e5857a6af939f4d06cae1103490970188215
dkdam <dkdam@live.com>
2022-08-09 22:15:05 +1000

    first commit

---
+++ src/components/Airlines.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Component } from "react";
+
+class Airlines extends Component {
+    render() {
+        return(
+            <div>
+                <marquee>
+                    <h1>Burning Airlines coming soon</h1>
+                </marquee>
+            </div>
+        )
+    }
+}
+
+export default Airlines
---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import Airlines from "./Airlines";
+
+function App() {
+  return (
+    <div className="App">
+      <Airlines />
+    </div>
+  );
+}
+
+export default App;

bc80fa20507caa5f14e7641909f557f903b4429d
rohan -fa <rohanfarzna@gmail.com>
2022-08-10 15:46:52 +1000

    login part

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,32 @@
-import Airlines from "./Airlines";
+import React, { Component, useState } from "react";
+import Login from "./Login";
 
 function App() {
+    const adminUser = {
+      email: 'jonesy@ga.co',
+      password: 'chicken'
+    }
+
+    const [user, setUser] = useState({name:'', email:''});
+    const [error, setError] = useState('');
+
+    const Log_in = details => {
+      console.log(details);
+    }
+    const Logout = () => {
+      console.log('Logout')
+    }
+
   return (
     <div className="App">
-      <Airlines />
+      {(user.email !='') ? (
+        <div className="welcome">
+          <h2>Welcome, <span>{ user.name }</span></h2>
+          <button>Logout</button>
+        </div>
+      ) : (
+        <Login Log_in={ Log_in } error={ error }/>
+      )}
     </div>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,47 @@
 import React, { Component } from "react";
 
 class Login extends Component {
+    constructor() {
+        super();
+        this.state = { name: '', email:'', password:''};
+        this._handleInput = this._handleInput.bind(this)
+        this._handleSubmit = this._handleSubmit.bind(this)
+    }
+    
+    _handleSubmit = (e) => {
+        e.preventDefault();
+        
+    }
+
+    _handleInput = (e) => {
+        this.setState( {content: e.target.value} )
+    }
+
+
     render()  {
         return(
-            <div>
-                <h1>Loging In Soon</h1>
-            </div>
+            <form onSubmit={ this._handleSubmit }>
+                <div>
+                    <h2>Login</h2>
+                    { /* Error */ }
+                    <div>
+                        <label htmlFor="name">Name:</label>
+                        <input type="text" name='name' id='name' />
+                    </div>
+                    <div>
+                        <label htmlFor="email">Email:</label>
+                        <input type="email" name='email' id='email' />
+                    </div>
+                    <div>
+                        <label htmlFor="password">Password:</label>
+                        <input type="password" name='password' id='password' />
+                    </div>
+                        <input type="submit" value='LOGIN'/>
+                </div>
+            </form>
+            
         )
     }
 }
 
-export default Login
+export default Login;

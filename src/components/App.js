8c1609122fc4b6a6a001457cded0c7c439567982
Nelson Feng <nelsonfeng98@gmail.com>
2022-08-10 13:04:49 +1000

    Added react routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,19 @@
 import Airlines from "./Airlines";
+import { Link, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Airlines />
-    </div>
+  <div>
+    <nav>
+      <Link to="/">Home</Link>
+      ||
+      <Link to="/airlines">Airlines</Link>
+    </nav>
+    <h1>Nothing to see here apparently</h1>
+    <Routes>
+      <Route path="/airlines" element={<Airlines />}/>
+    </Routes>
+  </div>
   );
 }
 

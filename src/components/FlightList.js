17ce87c544f9d898908ae0093871b47ca3524026
dkdam <dkdam@live.com>
2022-08-10 18:44:20 +1000

    flight search shows data

--- src/components/FlightList.js
+++ src/components/FlightList.js
@@ -1,16 +1,13 @@
 import React from "react";
 import axios from "axios";
 
-const SERVER_URL = "http://localhost:3000/flights"
-axios(SERVER_URL).then((results) => {
-    console.log(results)
-})
-
 const FlightList = (props) => {
     return (
         <div>
             <h3>Flight List</h3>
-            
+            {Object.keys(props.flights).map((key) => {
+                return <p>{key}: {props.flights[key]}</p>
+            })}
         </div>
     )
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -4,13 +4,42 @@ import SearchForm from "./SearchForm";
 import axios from "axios";
 
 class Search extends Component {
+    constructor() {
+        super()
+        this.state = ({
+            flight_number: [],
+            from: [],
+            to: [],
+            date: []
+        })
+        this.fetchInfo = this.fetchInfo.bind(this)
+    }
     
     fetchInfo(q) {
         console.log(`Search for ${q}`)
         const SERVER_URL = "http://localhost:3000/flights.json"
+        const newState = {
+            flight_number: [],
+            from: [],
+            to: [],
+            date: []
+        }
         axios(SERVER_URL).then((results) => {
-            console.log(results)
-            debugger
+            console.log(results);
+            for (let i = 0; i < results.data.length; i++) {
+                const flight_number = results.data[i].flight_number
+                newState.flight_number.push(flight_number)
+                const from = results.data[i].from
+                newState.from.push(from)
+                const to = results.data[i].to
+                newState.to.push(to)
+                const date = results.data[i].date
+                newState.date.push(date)
+            }
+            this.setState(newState)    
+            
+            // debugger
+            
         })
     }
     
@@ -18,7 +47,7 @@ class Search extends Component {
         return(
             <div>
                 <SearchForm onSubmit={ this.fetchInfo } />
-                <FlightList />
+                <FlightList flights = { this.state }/>
             </div>
         )
     }
